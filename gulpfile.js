f765b0c70809c53b45ef249117fa9c72730594bc
Oleksii Skopiuk <aztek2008@gmail.com>
2020-02-16 20:36:12 +0200

    Added sections till access-article for mobile (default) version

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,28 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var rename = require('gulp-rename');
-var autoprefixer = require('gulp-autoprefixer')
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var rename = require("gulp-rename");
+var autoprefixer = require("gulp-autoprefixer");
 
-function styles(sb) {
-    gulp.src('./scss/**/*.scss')
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .pipe(autoprefixer({
-            cascade: false
-        }))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('css/'))
-        .pipe
-    sb();
+function styles(cb) {
+  gulp
+    .src("./scss/**/*.scss")
+    .pipe(
+      sass({
+        outputStyle: "compressed"
+      })
+    )
+    .pipe(
+      autoprefixer({
+        cascade: false
+      })
+    )
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("css/"));
+  cb();
 }
 
-function watchSass() {
-  gulp.watch('./scss/**/*.scss', styles)
-}
+gulp.task("sass", styles);
 
-gulp.task('default', watchSass);
+gulp.task("sass:watch", function() {
+  gulp.watch("./scss/**/*.scss", styles);
+});
